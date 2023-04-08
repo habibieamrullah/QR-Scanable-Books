aa51a0da756b5abc729516ca122434907fb678af
Habibie <habibieamrullah@gmail.com>
2023-04-09 04:29:33 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
 
-$unm = "yamahdi";
-$pwd = "adrikni313";
-$baseurl = "https://scanbuku.perpusiccjakarta.id";
-$booksitetitle = "Kanz Digital Library";
+$unm = "admin";
+$pwd = "admin";
+$baseurl = "https://perpus.id";
+$booksitetitle = "Perpus";
 
 //Database connection
 $host = "localhost";
-$dbuser = "perq7182_scanbuku";
-$dbpassword = "anvrKa7I[guZ";
-$databasename = "perq7182_scanbuku";
+$dbuser = "user";
+$dbpassword = "password";
+$databasename = "db";
 
 $connection = mysqli_connect($host, $dbuser, $dbpassword, $databasename);
 $connection->set_charset("utf8"); 
