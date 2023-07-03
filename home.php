3f763d47149c039f5e051771efaedc94b8f056b6
Habibie <habibieamrullah@gmail.com>
2023-07-03 22:31:10 +0700

    Update home.php

--- home.php
+++ home.php
@@ -24,15 +24,77 @@
     <div style="display: table-cell; width: 10%; padding-right: 1em; box-sizing: border-box; text-align: center; color: gray;"><i class="fa fa-search"></i></div>
 </div>
 
-<div style="text-align: center; display: grid; grid-template-columns: auto auto auto;">
+
     <?php
-    $where = "";
+    //$where = "";
     
     if(isset($_GET["catid"])){
         $catid = mysqli_real_escape_string($connection, $_GET["catid"]);
+        echo "<h2 align='center'>Buku dalam Kategori " . mysqli_fetch_assoc(mysqli_query($connection, "SELECT * FROM categories WHERE id = $catid"))["title"] . "</h2>";
+        ?>
+        
+        <div style="text-align: center; display: grid; grid-template-columns: auto auto auto;">
+        <?php
+        
         $where = "WHERE category = $catid";
+        $sql = "SELECT * FROM books $where ORDER BY id DESC";
+        $result = mysqli_query($connection, $sql);
+        if($result){
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                    ?>
+                    <div class="thumbnailblock" style="width: 100%; height: 100%;">
+                        <a href="books.php?code=<?php echo $row["uniqid"] ?>"><img src="upl/<?php echo $row["slug"] ?>.jpg" style="box-sizing: border-box; padding: 0.5em; display: inline-block; vertical-align: middle; width: 100%; height: 100%; border-radius: 1em;"></a>
+                        <p style="display: none;"><?php echo $row["title"] ?></p>
+                    </div>
+                    <?php
+                }
+            }
+        }
+        
+        ?>
+        </div>
+        <?php
+    }else{
+        $result = mysqli_query($connection, "SELECT * FROM categories ORDER BY title ASC");
+        if($result){
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $catid = $row['id'];
+                    ?>
+                    <div>
+                        <div style="padding: 2em; text-align: center;">
+                            <h2>Buku <?php echo $row['title'] ?></h2>
+                        </div>
+                        <div style="text-align: center; display: grid; grid-template-columns: auto auto auto auto;">
+                        <?php
+                        $where = "WHERE category = $catid";
+                        $sql = "SELECT * FROM books $where ORDER BY id DESC";
+                        $cresult = mysqli_query($connection, $sql);
+                        if($cresult){
+                            if(mysqli_num_rows($cresult) > 0){
+                                while($crow = mysqli_fetch_assoc($cresult)){
+                                    ?>
+                                    <div class="thumbnailblock" style="width: 100%; height: 100%;">
+                                        <a href="books.php?code=<?php echo $crow["uniqid"] ?>"><img src="upl/<?php echo $crow["slug"] ?>.jpg" style="box-sizing: border-box; padding: 0.5em; display: inline-block; vertical-align: middle; width: 100%; height: 100%; border-radius: 1em;"></a>
+                                        <p style="display: none;"><?php echo $crow["title"] ?></p>
+                                    </div>
+                                    <?php
+                                }
+                            }
+                        }
+                        
+                        ?>
+                        </div>
+                        
+                    </div>
+                    <?php
+                }
+            }
+        }
     }
     
+    /*
     $sql = "SELECT * FROM books $where ORDER BY id DESC";
     $result = mysqli_query($connection, $sql);
     if($result){
@@ -40,15 +102,17 @@
             while($row = mysqli_fetch_assoc($result)){
                 ?>
                 <div class="thumbnailblock" style="width: 100%; height: 100%;">
-                    <a href="books.php?code=<?php echo $row["uniqid"] ?>"><img src="upl/<?php echo $row["slug"] ?>.jpg" style="box-sizing: border-box; padding: 0.5em; display: inline-block; vertical-align: middle; width: 100%; height: 100%;"></a>
+                    <a href="books.php?code=<?php echo $row["uniqid"] ?>"><img src="upl/<?php echo $row["slug"] ?>.jpg" style="box-sizing: border-box; padding: 0.5em; display: inline-block; vertical-align: middle; width: 100%; height: 100%; border-radius: 1em;"></a>
                     <p style="display: none;"><?php echo $row["title"] ?></p>
                 </div>
                 <?php
             }
         }
     }
+    */
+    
     ?>
-</div>
+
 
 <script>
     function toggleCatList(){
